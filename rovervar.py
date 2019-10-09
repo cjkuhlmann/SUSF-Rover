6ecc7b0ea5a898d2c607ecc76c3d91a97fe02f5f
cjkuhlmann <56342962+cjkuhlmann@users.noreply.github.com>
2019-10-09 19:32:26 +0100

    Update rovervar.py

--- rovervar.py
+++ rovervar.py
@@ -2,5 +2,5 @@
 #ALL ROVER VALUES MUST BE INTEGER OR FLOAT IN ORDER TO ALLOW COMPARISON UNLESS COMPARATOR IS CHANGED
 class RoverVar():
     def __init__(self,val,name):
-        self.value = val
-        self.name = name
+        self.value = val #value to be held by the rover (FLOAT or INT)
+        self.name = name #name to be used to identify the variable when sent via the network (preferably a 2-3 character identifier)
