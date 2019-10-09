645ac5734ddcb3d13b48111adb4abc059b7c6539
cjkuhlmann <56342962+cjkuhlmann@users.noreply.github.com>
2019-10-09 18:55:13 +0100

    Update arm.py

--- arm.py
+++ arm.py
@@ -3,7 +3,7 @@ from rovervar import *
 class Arm():
     def __init__(self):
         self.grabber_power = Rover_Var(0,"gp")
-        self.armX = Rover_Var(0,"ax")
-        self.armY = Rover_Var(0,"ay")
-        self.armZ = Rover_Var(0,"az")
+        self.arm_x = Rover_Var(0,"ax")
+        self.arm_y = Rover_Var(0,"ay")
+        self.arm_z = Rover_Var(0,"az")
         
