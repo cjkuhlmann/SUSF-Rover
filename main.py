2c78a15636cfe036376247f7b53ece7b1dc5fe27
cjkuhlmann <56342962+cjkuhlmann@users.noreply.github.com>
2019-10-10 23:45:31 +0100

    Update main.py
    Example update function and demonstration of how objects interact

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from controlstate import *
-from arm import *
-from drivetrain import *
-from rovervar import *
 from controller import *
 
 ps4_controller = Controller()
-control_state_a = ControlState()
-control_state_b = ControlState()
+control_state_new = ControlState()
+control_state_old = ControlState()
 ps4_controller.linked_to_control_state(control_state_a)
 
 
@@ -14,5 +11,7 @@ ps4_controller.linked_to_control_state(control_state_a)
 def update():
     ps4_controller.update()
     ps4_controller.set_linked_control_state()
-    calc_state_differential(control_state_a,control_state_b) #calculates differential where a is the new state
-    overwrite_control_state(control_state_b,control_state_a) #replaces b's values with a's so that b will continue to be the old state
+    calc_state_differential(control_state_new,control_state_old) #calculates differential where a is the new state
+    network_string = control_state_new.create_network_string()
+
+
