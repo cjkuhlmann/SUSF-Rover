64b89935e09aee2ec74556ba2f10fd4041c31b99
cjkuhlmann <56342962+cjkuhlmann@users.noreply.github.com>
2019-10-10 23:41:49 +0100

    Delete drivetrain.py

--- drivetrain.py
+++
@@ -1,7 +0,0 @@
-from rovervar import *
-#------------------------------------Example of Drivetrain variables--------------------------------------------------
-
-class Drivetrain():
-    def __init__(self):
-        self.left_power = RoverVar(0,"lp")
-        self.right_power = RoverVar(0,"rp")
