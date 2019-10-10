20cffff036490fc34194b5c9320d07c4fba6e218
cjkuhlmann <56342962+cjkuhlmann@users.noreply.github.com>
2019-10-10 23:41:41 +0100

    Delete arm.py

--- arm.py
+++
@@ -1,9 +0,0 @@
-from rovervar import *
-#--------------------------------example of arm variable--------------------------------------
-class Arm():
-    def __init__(self):
-        self.grabber_power = RoverVar(0,"gp")
-        self.arm_x = RoverVar(0,"ax")
-        self.arm_y = RoverVar(0,"ay")
-        self.arm_z = RoverVar(0,"az")
-        
